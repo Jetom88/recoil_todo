9093380270b2cdd2da814469180a83f16a2cfff4
hyeri <shinhro3o@gmail.com>
2022-12-01 17:53:52 +0900

    필요없는 selector 삭제

--- src/states/selector.js
+++ src/states/selector.js
@@ -15,10 +15,3 @@ export const todoSelector = selector({
     set(todoIdState, (id) => id + 1);
   },
 });
-
-export const isShowBtn = selector({
-  key: "isShowBtn",
-  get: ({ get }) => {
-    get(Boolean);
-  },
-});
