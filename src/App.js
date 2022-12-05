e662a212dd7074696367cbb97b7cc7888d7db47a
hyeri <shinhro3o@gmail.com>
2022-12-05 15:50:53 +0900

    zustand 공부중

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import Todo from "./components/Todo";
+// import Todo from "./components/Todo";
+import { useStore } from "./store/store";
 
 function App() {
-  return <Todo />;
+  // return <Todo />;
+  const bears = useStore((state) => state.bears);
+  const increase = useStore((state) => state.increase);
+  const decrease = useStore((state) => state.decrease);
+  return (
+    <>
+      <h1>{bears}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </>
+  );
 }
 
 export default App;
