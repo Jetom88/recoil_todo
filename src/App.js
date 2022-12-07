70eb5f7c08dbae01b505a47aac589e2e74ae399d
hyeri <shinhro3o@gmail.com>
2022-12-07 14:21:53 +0900

    todo list 완성

--- src/App.js
+++ src/App.js
@@ -4,27 +4,98 @@ import { useCallback, useEffect, useState } from "react";
 import useStore from "./hook/useStore";
 import styles from "./style/zustandSt.module.scss";
 
-const ItemEdit = ({ item, onDelete }) => {
+const ItemEdit = ({ item, onDelete, getData }) => {
   const [edit, setEdit] = useState(false);
 
-  console.log(edit, "test");
+  const [form, setForm] = useState({
+    title: "",
+    content: "",
+  });
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const onClick = async (e) => {
+    const { title, content } = form;
+
+    await fetch(`${URL}/todo/${e}`, {
+      method: "PUT",
+
+      body: new URLSearchParams({
+        title,
+        content,
+      }),
+    });
+
+    setForm({ title: "", content: "" });
+    setEdit(false);
+    getData();
+  };
+
   return (
-    <div className={styles.content}>
-      <div>{edit ? <input /> : <p>{item.title}</p>}</div>
-      <p>{item.content}</p>
+    <div className={styles.todo}>
+      {edit && (
+        <div className={styles.popup}>
+          <div className={styles.popupTitle}>
+            <h3>Edit todo</h3>
+            <button
+              className={styles.close}
+              onClick={() => {
+                setEdit(false);
+              }}
+            >
+              ✕
+            </button>
+          </div>
+          <input
+            name="title"
+            value={form.title}
+            onChange={onChange}
+            placeholder="title"
+          />
+          <input
+            name="content"
+            value={form.content}
+            onChange={onChange}
+            placeholder="content"
+          />
+
+          <button
+            type="submit"
+            onClick={() => {
+              onClick(item.id);
+            }}
+            className={styles.submitBtn}
+          >
+            edit
+          </button>
+        </div>
+      )}
+
+      <div className={styles.todoContent}>
+        <p className={styles.todoTitle}>{item.title}</p>
+        <p className={styles.content}>{item.content}</p>
+      </div>
+
       <button
         onClick={() => {
           setEdit(!edit);
         }}
       >
-        edit
+        📝
       </button>
       <button
         onClick={() => {
           onDelete(item.id);
         }}
       >
-        delete
+        🗑️
       </button>
     </div>
   );
@@ -40,6 +111,8 @@ function App() {
     title: "",
     content: "",
   });
+
+  const [show, setShow] = useState(false);
 
   const getData = useCallback(async () => {
     const res = await fetch(`${URL}/todo`).then((res) => res.json());
@@ -69,7 +142,7 @@ function App() {
     });
 
     setForm({ title: "", content: "" });
-
+    setShow(false);
     getData();
   };
 
@@ -86,17 +159,66 @@ function App() {
   }, [getData]);
 
   return (
-    <div>
-      {apiData?.map((apiData) => (
-        <ItemEdit key={apiData.id} item={apiData} onDelete={onDelete} />
-      ))}
+    <div className={styles.container}>
+      {show && (
+        <div className={styles.popup}>
+          <div className={styles.popupTitle}>
+            <h3>Add todo</h3>
+            <button
+              className={styles.close}
+              onClick={() => {
+                setShow(false);
+              }}
+            >
+              ✕
+            </button>
+          </div>
+          <input
+            name="title"
+            value={form.title}
+            onChange={onChange}
+            placeholder="title"
+          />
+          <input
+            name="content"
+            value={form.content}
+            onChange={onChange}
+            placeholder="content"
+          />
 
-      <input name="title" value={form.title} onChange={onChange} />
-      <input name="content" value={form.content} onChange={onChange} />
+          <button type="submit" onClick={onClick} className={styles.submitBtn}>
+            submit
+          </button>
+        </div>
+      )}
+      <div className={styles.date}>
+        <h3 className={styles.mainText}>TODO LIST</h3>
 
-      <button type="submit" onClick={onClick}>
-        submit
-      </button>
+        <div className={styles.addTodo}>
+          <button
+            className={styles.btn}
+            onClick={() => {
+              setShow(!show);
+            }}
+          >
+            +
+          </button>
+        </div>
+      </div>
+
+      {apiData.length !== 0 ? (
+        <div className={styles.todoBorder}>
+          {apiData?.map((apiData) => (
+            <div className={styles.todoList} key={apiData.id}>
+              <ItemEdit item={apiData} onDelete={onDelete} getData={getData} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className={styles.noData}>
+          <p>할일이 없나여?🥲</p>
+        </div>
+      )}
     </div>
   );
 }
